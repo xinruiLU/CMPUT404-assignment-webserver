60c53ab4ac93982618cb171ffb5e9999afa1b276
xinruiLU <xinrui7@ualberta.ca>
2019-01-25 15:57:39 -0700

    demo1

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +29,66 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(1024).strip().decode('utf-8')
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        the_file = self.data.split()[1]
+        the_request = self.data.split()[0]
+        #self.request.sendall(bytearray("OK",'utf-8'))
+        current_fpath = os.path.abspath(__file__)
+        print(current_fpath)
+        name_fpath = os.path.dirname(current_fpath)
+        path = name_fpath+"/www"+the_file
+        print (path)
+
+        if (os.path.abspath(path)):
+            if  not "/www" in path:
+                self.send_404(self.request)
+                return
+        else:
+            self.send_404(self.request)
+            return
+
+
+        if the_request=="GET":
+
+            if os.path.isfile(path):
+                get_type = path.split(".")[-1].lower()
+                if(get_type == "css" or get_type == "html"):
+                    get_type = "text/"+get_type
+                    self.send_200_OK(self.request,path,name_fpath,get_type)
+                else:
+                    self.send_404(self.request)
+            elif os.path.isdir(path):
+                if the_file.endswith("/"):
+                    path = path + "index.html"
+                else:
+                    path = path + "/index.html"
+                if os.path.isfile(path):
+                    get_type = "text/html\n\n"
+                    self.send_200_OK(self.request,path,name_fpath,get_type)
+            else:
+                self.send_404(self.request)
+        else:
+            self.send_405(self.request)
+    def send_404(self,request):
+        self.request.sendall(bytearray("HTTP/1.1 404 Not Found \r\n",'utf-8'))
+        return
+
+    def send_200_OK(self,request,path,name_fpath,get_type):
+
+        open_file = open(path)
+
+        self.request.sendall(bytearray("HTTP/1.1 200 OK \r\nContent-Type: "+ get_type+
+        ";Transfer-Encoding: chunked;\r\n\r\n"+open_file.read(1024),'utf-8'))
+        open_file.close()
+        return
+
+    def send_405(self,request):
+        self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed \r\n",'utf-8'))
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
